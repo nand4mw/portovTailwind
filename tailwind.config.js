fa359080973a97f985f22cb416fa8131d2002c4b
4nndmw <anandamaulanaw1850@gmail.com>
2023-08-17 21:10:01 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}", "index.html"],
+  content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
     fontFamily: {
       body: ["Segoe UI", "Roboto", "Open Sans", "Helvetica Neue"],
